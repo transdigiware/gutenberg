dd6a5f916b995e2202de9b59891595d0eaaf44cb
Jorge <jorge.costa@developer.pt>
2018-10-18 13:03:34 +0100

    Fix: Another paragraph is inserted, inside the block even if the block template already includes a paragraph.

--- packages/editor/src/components/inner-blocks/index.js
+++ packages/editor/src/components/inner-blocks/index.js
@@ -23,7 +23,9 @@ import { withBlockEditContext } from '../block-edit/context';
 class InnerBlocks extends Component {
 	constructor() {
 		super( ...arguments );
-
+		this.state = {
+			templateInProcess: !! this.props.template,
+		};
 		this.updateNestedSettings();
 	}
 
@@ -39,7 +41,12 @@ class InnerBlocks extends Component {
 		const { innerBlocks } = this.props.block;
 		// only synchronize innerBlocks with template if innerBlocks are empty or a locking all exists
 		if ( innerBlocks.length === 0 || this.getTemplateLock() === 'all' ) {
-			return 	this.synchronizeBlocksWithTemplate();
+			this.synchronizeBlocksWithTemplate();
+		}
+		if ( this.state.templateInProcess ) {
+			this.setState( {
+				templateInProcess: false,
+			} );
 		}
 	}
 
@@ -100,6 +107,7 @@ class InnerBlocks extends Component {
 			isSmallScreen,
 			isSelectedBlockInRoot,
 		} = this.props;
+		const { templateInProcess } = this.state;
 
 		const classes = classnames( 'editor-inner-blocks', {
 			'has-overlay': isSmallScreen && ! isSelectedBlockInRoot,
@@ -107,10 +115,12 @@ class InnerBlocks extends Component {
 
 		return (
 			<div className={ classes }>
-				<BlockList
-					rootClientId={ clientId }
-					{ ...{ layouts, allowedBlocks, templateLock, template } }
-				/>
+				{ ! templateInProcess && (
+					<BlockList
+						rootClientId={ clientId }
+						{ ...{ layouts, allowedBlocks, templateLock, template } }
+					/>
+				) }
 			</div>
 		);
 	}
